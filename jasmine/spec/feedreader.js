bd48e9f6b9aa9620f2441a8b076bc4bfeb62eeb3
Lucia Gonzalez Moscoso <luciagonzalezmoscoso@gmail.com>
2018-04-27 17:32:29 -0700

    Add test to ensure that loadFeed function works

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -73,15 +73,28 @@ $(function() {
             expect($('body').hasClass('menu-hidden')).toBeTruthy();
          });
     });
-    /* TODO: Write a new test suite named "Initial Entries" */
 
+
+    /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
 
+        it('exists at least one entry', function() {
+            expect($('.feed .entry').length).toBeGreaterThan(0);
+        });
+    });
+
+    
     /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
